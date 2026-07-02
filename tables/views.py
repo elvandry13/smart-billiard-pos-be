8bc1b3d70e9a4f3a9b22aee067d32f2c01869711
Elvandry Ghiffary <elvandry13@gmail.com>
2026-07-02 22:58:44 +0700

    fix: Outlet scoping can be bypassed, breaking the outlet/table_type invariant.

--- tables/views.py
+++ tables/views.py
@@ -23,7 +23,24 @@ class OutletScopedViewSet(viewsets.ModelViewSet):
             return qs
         return qs.filter(outlet=user.outlet)
 
+    def get_serializer(self, *args, **kwargs):
+        user = self.request.user
+        if not user.is_super_admin:
+            data = kwargs.get('data')
+            if data is not None:
+                data = data.copy()
+                data['outlet'] = user.outlet_id
+                kwargs['data'] = data
+        return super().get_serializer(*args, **kwargs)
+
     def perform_create(self, serializer):
+        user = self.request.user
+        if user.is_super_admin:
+            serializer.save()
+        else:
+            serializer.save(outlet=user.outlet)
+
+    def perform_update(self, serializer):
         user = self.request.user
         if user.is_super_admin:
             serializer.save()
